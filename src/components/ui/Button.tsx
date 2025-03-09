f1715fbc687093093524e9f29759d169b98940b4
sajid-edninja <ssk73573@gmail.com>
2025-03-09 20:38:35 +0530

    fix button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,18 +1,20 @@
+import { ReactElement } from "react";
+
 export interface ButtonProps {
   variant: "primary" | "secondary";
   size: "sm" | "md" | "lg";
   text: string;
-  startIcon?: any;
-  endIcon?: any;
+  startIcon?: ReactElement;
+  endIcon?: ReactElement;
   onClick: () => void;
 }
 
 const defaultStyles =
-  "rounded-md px-[16px] py-[12px] flex gap-[8px] items-center";
+  "rounded-md px-4 py-2 flex gap-[8px] items-center font-light";
 
 const variantStyles = {
   primary: "bg-purple-600 text-white",
-  secondary: "bg-purple-400 text-purple-600",
+  secondary: "bg-purple-200 text-purple-600",
 };
 
 const sizeStyles = {
