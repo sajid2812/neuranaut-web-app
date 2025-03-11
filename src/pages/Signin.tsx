b0b173054a4eef28200194e97bf93955a9766846
sajid-edninja <ssk73573@gmail.com>
2025-03-11 09:25:48 +0530

    add signin page

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,14 +1,14 @@
 import { Button } from "../components/ui/Button";
 import { Input } from "../components/ui/Input";
 
-export function Signup() {
+export function Signin() {
   return (
     <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
       <div className="bg-white rounded-xl border min-w-48">
         <Input placeholder="Username" />
         <Input placeholder="Password" />
         <div className="flex justify-center pt-4">
-          <Button variant="primary" text="Signup" fullWidth={true} />
+          <Button variant="primary" text="Signin" fullWidth={true} />
         </div>
       </div>
     </div>
