d6aead6bcd1211ab86f9573ec814270d475cd8c5
sajid-edninja <ssk73573@gmail.com>
2025-03-14 23:47:38 +0530

    fix signup

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,12 +1,21 @@
+import { useRef } from "react";
 import { Button } from "../components/ui/Button";
 import { Input } from "../components/ui/Input";
 
 export function Signup() {
+  const usernameRef = useRef<any>(null);
+  const passwordRef = useRef<any>(null);
+
+  function signup() {
+    const username = usernameRef.current?.value;
+    const password = passwordRef.current?.value;
+  }
+
   return (
     <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
       <div className="bg-white rounded-xl border min-w-48">
-        <Input placeholder="Username" />
-        <Input placeholder="Password" />
+        <Input ref={usernameRef} placeholder="Username" />
+        <Input ref={passwordRef} placeholder="Password" />
         <div className="flex justify-center pt-4">
           <Button variant="primary" text="Signup" fullWidth={true} />
         </div>
