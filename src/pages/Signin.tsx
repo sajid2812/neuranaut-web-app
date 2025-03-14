338e5d509e59a19a8adddf194ae6373065965e3a
sajid-edninja <ssk73573@gmail.com>
2025-03-14 23:59:22 +0530

    fix signin

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,7 +1,11 @@
+import { useRef } from "react";
 import { Button } from "../components/ui/Button";
 import { Input } from "../components/ui/Input";
 
 export function Signin() {
+  const usernameRef = useRef<any>(null);
+  const passwordRef = useRef<any>(null);
+
   return (
     <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
       <div className="bg-white rounded-xl border min-w-48">
